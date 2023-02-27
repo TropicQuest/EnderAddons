c376c07fb867ffaba9262c038c525839cee20826
TropicQuest <tnt360viva@outlook.com>
2023-02-26 19:08:53 -0800

    Initial commit

---
+++ src/main/java/net/tropicquest/enderaddons/EnderAddonsClient.java
@@ -0,0 +1,10 @@
+package net.tropicquest.enderaddons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnderAddonsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
