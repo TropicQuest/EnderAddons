3d79900a75035c9a41acf7647a6e5f2221813c83
TropicQuest <tnt360viva@outlook.com>
2023-03-02 22:42:54 -0800

    worldgen done now i gotta figure out how to release it.

--- src/main/java/net/tropicquest/enderaddons/EnderAddons.java
+++ src/main/java/net/tropicquest/enderaddons/EnderAddons.java
@@ -2,9 +2,15 @@ package net.tropicquest.enderaddons;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import net.tropicquest.enderaddons.block.ModBlocks;
 import net.tropicquest.enderaddons.item.ModItemGroup;
 import net.tropicquest.enderaddons.item.ModItems;
@@ -19,6 +25,8 @@ public class EnderAddons implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("enderaddons");
+	public static final RegistryKey<PlacedFeature> CUSTOM_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("enderaddons","endershard_ore"));
+
 
 	@Override
 	public void onInitialize() {
@@ -31,6 +39,7 @@ public class EnderAddons implements ModInitializer {
 		Registry.register(Registries.ITEM, new Identifier("enderaddons", "enderhoe"), ENDERHOE);
 		Registry.register(Registries.ITEM, new Identifier("enderaddons", "endershovel"), ENDERSHOVEL);
 		Registry.register(Registries.ITEM, new Identifier("enderaddons", "enderaxe"), ENDERAXE);
+		BiomeModifications.addFeature(BiomeSelectors.foundInTheEnd(), GenerationStep.Feature.UNDERGROUND_ORES, CUSTOM_ORE_PLACED_KEY);
 
 	}
 }
